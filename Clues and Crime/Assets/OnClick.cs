e94e4e2f3b0e87fa218e7fa241e2ecbf3f8ebb46
Noah Buckwalter <buckwalter59@gmail.com>
2024-10-31 23:05:58 -0400

    Added UI navigation
    Button clicks hide/show UI panels, allowing the user to navigate the computer interface.

--- Clues and Crime/Assets/OnClick.cs
+++ Clues and Crime/Assets/OnClick.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class OnClick : MonoBehaviour
 {
 
-    public GameObject onClickWindow;
+    public GameObject BackgroundPanel;
+    public GameObject Panel1;
+    public GameObject Panel2;
+
+    // Boolean for tracking if background panel is active
+    private bool BGActive;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        BGActive = true;
     }
 
     // Update is called once per frame
@@ -23,6 +29,17 @@ public class OnClick : MonoBehaviour
         Debug.Log("Button clicked.");
 
         // Experimenting with best ways to popup window, scene change, UI layers, etc. -Noah
-        // onClickWindow.SetActive(true); // One way to hide/show UI elements
+        if (BGActive)
+        {
+            BGActive = false;
+            BackgroundPanel.SetActive(false); // Hide background panel
+            Panel1.SetActive(true); // Show new window panel
+        }
+        else
+        {
+            BGActive = true;
+            BackgroundPanel.SetActive(true);
+            Panel1.SetActive(false);
+        }
     }
 }
